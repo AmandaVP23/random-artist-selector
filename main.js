593f26870a6222dfd227c20017c79849309af559
Amanda Vieira <amanda.vieira@voidsoftware.com>
2022-12-04 14:33:05 +0000

    Added more and removed duplicated

--- main.js
+++ main.js
@@ -12,13 +12,21 @@ function getRandomArtist() {
     artistNameEl.textContent = artists[randomIdx];
 }
 
+function onlyUnique(value, index, self) {
+    if (self.indexOf(value) !== index) console.log('v', value);
+    return self.indexOf(value) === index;
+}
+
 function setup() {
     fetch('./artistsList.json')
         .then((response) => response.json())
         .then((json) => {
+            console.log(json.artists.length)
             artists = json.artists.sort((a, b) => {
                 return a.localeCompare(b, undefined, { sensitivity: 'base' })
-            });
+            }).filter(onlyUnique);
+
+            console.log(artists.length)
 
             console.log(artists.join('", \n"'))
         });
